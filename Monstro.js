a64785cc665d067941baceaa54a0cf6354c59bd1
luisfelipe1005 <47942553+luisfelipe1005@users.noreply.github.com>
2024-12-30 14:35:07 +0000

    .

--- Monstro.js
+++ Monstro.js
@@ -1,7 +1,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
-  import {getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+  import {getAuth, createUserWithEmailAndPassword, validatePassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,18 +23,36 @@
   const analytics = getAnalytics(app);
   const auth = getAuth(app);
 
+  const status = await validatePassword(getAuth(), passwordFromUser);
+  if (!status.isValid) {
+    // Password could not be validated. Use the status to show what
+    // requirements are met and which are missing.
+  
+    // If a criterion is undefined, it is not required by policy. If the
+    // criterion is defined but false, it is required but not fulfilled by
+    // the given password. For example:
+    const needsLowerCase = status.containsLowercaseLetter !== true;
+    const needsUpperCase = status.containsUppercaseLetter !== true;
+    const needsNumber = status.containsNumber !== true;
+
+    console.log("Requisitos faltando:");
+    if (needsLowerCase) console.log("- Precisamos de uma letra minúscula.");
+    if (needsUpperCase) console.log("- Precisamos de uma letra maiúscula.");
+    if (needsNumber) console.log("- Precisamos de um número.");
+   else {
+    console.log("Senha válida!");
+  }
+}
+
   //botão de submit
 
   const submit = document.getElementById('submit');
   submit.addEventListener("click",function(event){
-    alert(1)
 event.preventDefault()
-alert(2)
 //inputs para login
 
 const email = document.getElementById('email').value;
 const senha=document.getElementById('senha').value;
-alert(3)
 createUserWithEmailAndPassword(auth, email, senha)
 .then((userCredential) => {
 // Signed up
